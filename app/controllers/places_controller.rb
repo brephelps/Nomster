41629c104b2e9844b6e035e25924deffd347a296
Bre Phelps <bphelps@live.com>
2019-07-21 23:19:10 +0000

    form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,14 @@
 class PlacesController < ApplicationController
 
+  def Kaminari.configure
+    paginates_per 10
+    max_paginates_per 10
+  end
   def index
-    @places = Place.all
+    @places = Place.all.order(:name).page params[:page]
+  end
+
+  def new
+    @place = Place.new
   end
 end
