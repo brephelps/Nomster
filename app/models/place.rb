8477419f8f9717f19a2346698c3cad1ebe65ebab
Bre Phelps <bphelps@live.com>
2019-07-24 21:03:44 +0000

    Geocoding

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,9 @@
 class Place < ApplicationRecord
-  validates :name, presence: true
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
+
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :name, presence: true, length: { minimum: 3 }
 end
