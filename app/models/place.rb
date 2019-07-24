de6f5d736b440e071d276595b14b5ab90c98500f
Bre Phelps <bphelps@live.com>
2019-07-24 22:05:45 +0000

    comments db

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
   geocoded_by :address
   after_validation :geocode
+
 
   validates :address, presence: true
   validates :description, presence: true
