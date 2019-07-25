1bdceef30eaa0747b2b48e9353a3ac1ec1e4490e
Bre Phelps <bphelps@live.com>
2019-07-25 00:06:07 +0000

    update index page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,11 +5,11 @@ class Comment < ApplicationRecord
   validates :message, presence: true
 
   RATINGS = {
-    'one star': '1_star',
-    'two stars': '2_stars',
-    'three stars': '3_stars',
-    'four stars': '4_stars',
-    'five stars': '5_stars'
+    'One Star': '1_star',
+    'Two Stars': '2_stars',
+    'Three Stars': '3_stars',
+    'Four Stars': '4_stars',
+    'Five Stars': '5_stars'
   }
 
   def humanized_rating
