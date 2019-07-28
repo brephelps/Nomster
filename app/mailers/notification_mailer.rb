523f09ffe27c9843d95796de8e92c87831074d21
Bre Phelps <bphelps@live.com>
2019-07-28 17:07:28 +0000

    emails

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "bphelps@live.com",
+    mail(to: "phelpsbrianna4@gmail.com",
          subject: "A comment has been added to your place")
   end
 end
